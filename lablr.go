41550fb16ba94737310e20c86ac5c5428c36332f
Thanabodee <thanabodee@osdev.co.th>
2014-03-19 16:36:13 +0700

    close #1 - parsing xml to model object

---
+++ lablr.go
@@ -0,0 +1,28 @@
+package lablr
+
+import "encoding/xml"
+
+type Model struct {
+	Name        string `xml:"name,attr"`
+	Description string `xml:"description"`
+	Version     string `xml:"version"`
+	Author      string `xml:"author"`
+	Types       []Type `xml:"types>type"`
+}
+
+type Type struct {
+	Name       string     `xml:"name,attr"`
+	Title      string     `xml:"title"`
+	Properties []Property `xml:"properties>property"`
+}
+
+type Property struct {
+	Name string `xml:"name,attr"`
+}
+
+func NewModel(modelString string) (model Model, err error) {
+	model = Model{}
+	err = xml.Unmarshal([]byte(modelString), &model)
+
+	return
+}
